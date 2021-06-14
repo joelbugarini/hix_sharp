4ce0b04ab5a5fcb94798204d52409f71afa9c332
joe <joelbugarini@gmail.com>
2021-06-13 20:34:50 -0700

    JsonParser Added

---
+++ DatabaseClasses/Schema.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+using System.Text;
+
+
+namespace hix
+{
+    public class Schema
+    {
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        private List<Table> tables;
+
+        internal List<Table> Tables
+        {
+            get { return tables; }
+            set { tables = value; }
+        }
+
+        private string content;
+
+    }
+}
